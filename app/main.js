7d87173173142618bd28e585a5f9e95f8383d2ff
Roman Fedorenkov <suffering123494@yandex.ru>
2015-11-24 13:17:04 +0300

    feat(app): rewrite on es6

--- app/main.js
+++ app/main.js
@@ -1,12 +1,15 @@
+import reflectMetadata from 'reflect-metadata';
+import {ComponentMetadata, ViewMetadata, bootstrap} from 'angular2/angular2';
+
 function AppComponent() {}
 AppComponent.annotations = [
-  new angular.ComponentAnnotation({
+  new ComponentMetadata({
     selector: 'app'
   }),
-  new angular.ViewAnnotation({
+  new ViewMetadata({
     template: '<h1>My first Angular 2 App</h1>'
   })
 ];
 document.addEventListener('DOMContentLoaded', function() {
-  angular.bootstrap(AppComponent);
+  bootstrap(AppComponent);
 });
